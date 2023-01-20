1f0db8338602249a9080002d9abfabbe19dc4837
maesterzak <abkdking1703@gmail.com>
2023-01-20 11:21:32 +0200

    update host

--- texkoop/settings.py
+++ texkoop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.up.railway.app', 'truebackend.texkoop.com']
 
 
 # Application definition
