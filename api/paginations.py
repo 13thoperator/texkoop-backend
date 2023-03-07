2cd7f2f06f9876058d8e6920e49161b033d1e560
Bitee-cd <Calebduniya45@gmail.com>
2023-03-07 10:12:47 +0100

    changed pagination size

--- api/paginations.py
+++ api/paginations.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(pagination.PageNumberPagination):
 
-    page_size = 2
+    page_size = 8
     page_query_param = 'p'
     page_size_query_param = 'page_size'
     limit_query_param='l'
